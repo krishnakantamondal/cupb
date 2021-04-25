f29b62f4eee3c266bae99bdfe03a1a3ecb293bb2
krishnakanta <krishnakanta1987@gmail.com>
2021-04-25 22:43:21 +0530

    added Fn

--- fun_fn.c
+++ fun_fn.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
-int Fn(float x)  // correct the data return type of the function
-{
-return (4*x) ;
-}
+
+float Fn(float x);
 
 int main()
 {
 float x=2.5;
 printf("%f", Fn(x));
 
+}
+
+float Fn(float x)  // correct the data return type of the function
+{
+return (4*x) ;
 }
